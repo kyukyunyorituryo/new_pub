b7f8cfb479295600e01f1653ce9c44c35f104047
kyukyunyorituryo <kyukyunyorituryo@live.jp>
2020-04-22 21:17:50 +0900

    json化

--- js/new_pub.js
+++ js/new_pub.js
@@ -1,6 +1,6 @@
 templete='            <li class="media mb-3 alert-warning ">\n            <a href="<%= url %>">\n                <img src="<%= image_url %>" width="158" height="250" class="mr-3"></a>\n                <div class="media-body">\n                    <h5><%= title %></h5>\n                    <p>価格：<%= price %>円</p>\n                    <p><%= contributor %></p>\n                    <p><%= publisher %></p>\n\n                </div>\n            </li>'
 nav=[];
-function templeterender(){
+function templeterender(items){
 for (let i = 0; i < items.length; i++) {
     nav[i] = ejs.render(templete, {
         title: items[i].Title,
@@ -12,10 +12,25 @@ for (let i = 0; i < items.length; i++) {
     })
     }}
 
-document.addEventListener("DOMContentLoaded", function(){
-    console.log(nav)
+function getJSON(json_data) {
+	var req = new XMLHttpRequest();
+	req.onreadystatechange = function() {
+		if(req.readyState == 4 && req.status == 200){
+			var data = JSON.parse(req.responseText);
+			items = data	;
+		}
+	};
+	req.open("GET",json_data, false);
+	req.send(null);
+}
 
-    templeterender()
+document.addEventListener("DOMContentLoaded", function(){
+//    console.log(nav)
+
+var items
+json_data= "20200423j.json"
+getJSON(json_data)
+    templeterender(items)
     for (let i = 0; i < nav.length; i++) {$('#frame').append(nav[i]);}
 
   });
