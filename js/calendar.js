506f39851513130eb1897ca92c576e4fbc991658
急急如律令 <kyukyunyorituryo@live.jp>
2024-07-09 22:43:15 +0900

    Updates

---
+++ js/calendar.js
@@ -0,0 +1,26 @@
+            $(function () {
+                $('#datetimepicker1').datetimepicker({
+               minDate:'2023/01/01',
+               maxDate:'2024/09/30',
+            dayViewHeaderFormat: 'YYYY年 MMMM',
+            tooltips: {
+                close: '閉じる',
+                selectMonth: '月を選択',
+                prevMonth: '前月',
+                nextMonth: '次月',
+                selectYear: '年を選択',
+                prevYear: '前年',
+                nextYear: '次年',
+                selectTime: '時間を選択',
+                selectDate: '日付を選択',
+                prevDecade: '前期間',
+                nextDecade: '次期間',
+                selectDecade: '期間を選択',
+                prevCentury: '前世紀',
+                nextCentury: '次世紀'
+            },
+            format: 'YYYYMMDD',
+            locale: 'ja',
+            showClose: true
+        });
+            });
